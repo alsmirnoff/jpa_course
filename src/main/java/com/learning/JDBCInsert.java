03ec13d137aae5e7208567f54df14eae527e8c47
alsmirnoff <alex-sm-91@mail.ru>
2025-06-17 18:11:36 +0300

    lesson 2 jdbc init

---
+++ src/main/java/com/learning/JDBCInsert.java
@@ -0,0 +1,33 @@
+package com.learning;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JDBCInsert {
+
+    static final String DB_URL = "jdbc:mysql://localhost:3306/test_db";
+    static final String USER = "jpauser";
+    static final String PWD = "jpapwd";
+
+    public static void main(String[] args) {
+
+        Student student = new Student("Chanel", "King", 9.1);
+        try(Connection connection = DriverManager.getConnection(DB_URL, USER, PWD)){
+            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO students(name, surname, avg_grade) VALUES (?, ?, ?)")) {
+                statement.setString(1, student.getName());
+                statement.setString(2, student.getSurname());
+                statement.setDouble(3, student.getAvgGrade());
+                statement.executeUpdate();
+                statement.close();
+            } catch(SQLException e) {
+                e.printStackTrace();
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
